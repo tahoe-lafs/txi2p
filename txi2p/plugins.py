224b1d8584c53106a0ba35b7d4cec53a13a563ab
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-10-11 09:45:02 -0400

    There are only "new-style" classes now
    Inheriting from `object` is redundant and unusual.

--- txi2p/plugins.py
+++ txi2p/plugins.py
@@ -1,7 +1,6 @@
 # Copyright (c) str4d <str4d@mail.i2p>
 # See COPYING for details.
 
-from builtins import object
 from twisted.internet.endpoints import clientFromString
 from twisted.internet.interfaces import IStreamClientEndpointStringParserWithReactor
 from twisted.internet.interfaces import IStreamServerEndpointStringParser
@@ -21,7 +20,7 @@ def _parseOptions(options):
     return dict([option.split(':') for option in options.split(',')]) if options else {}
 
 @implementer(IPlugin, IStreamClientEndpointStringParserWithReactor)
-class I2PClientParser(object):
+class I2PClientParser:
     prefix = 'i2p'
 
     def _parseBOBClient(self, reactor, host, port, bobEndpoint,
@@ -66,7 +65,7 @@ class I2PClientParser(object):
 
 
 @implementer(IPlugin, IStreamServerEndpointStringParser)
-class I2PServerParser(object):
+class I2PServerParser:
     prefix = 'i2p'
 
     def _parseBOBServer(self, reactor, keyfile, port, bobEndpoint,
